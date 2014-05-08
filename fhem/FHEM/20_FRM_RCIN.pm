6f886f1c4b725c9f339e506104c6294e4bad25f5
git-developer <j@ckware.net>
2014-05-08 07:01:47 +0200

    Added support for RC receivers

--- fhem/FHEM/20_FRM_RCIN.pm
+++ fhem/FHEM/20_FRM_RCIN.pm
@@ -18,15 +18,10 @@ use Device::Firmata::Constants  qw/ :all /;
 #####################################
 
 my %sets = (
-  "tristateCode"     => $Device::Firmata::Protocol::RCOUTPUT_COMMANDS->{RCOUTPUT_CODE_TRISTATE},
-#  "longCode"         => $Device::Firmata::Protocol::RCOUTPUT_COMMANDS->{RCOUTPUT_CODE_LONG},
-#  "charCode"         => $Device::Firmata::Protocol::RCOUTPUT_COMMANDS->{RCOUTPUT_CODE_CHAR},
 );
 
 my %attributes = (
-  "protocol"         => $Device::Firmata::Protocol::RCOUTPUT_COMMANDS->{RCOUTPUT_PROTOCOL},
-  "pulseLength"      => $Device::Firmata::Protocol::RCOUTPUT_COMMANDS->{RCOUTPUT_PULSE_LENGTH},
-  "repeatTransmit"   => $Device::Firmata::Protocol::RCOUTPUT_COMMANDS->{RCOUTPUT_REPEAT_TRANSMIT},
+  "tolerance"        => $Device::Firmata::Protocol::RCINPUT_COMMANDS->{RCINPUT_TOLERANCE},
 );
 
 my %tristateBits = (
@@ -36,30 +31,29 @@ my %tristateBits = (
 );
 
 sub
-FRM_RCOUT_Initialize($)
+FRM_RCIN_Initialize($)
 {
   my ($hash) = @_;
 
-  $hash->{SetFn}     = "FRM_RCOUT_Set";
   $hash->{DefFn}     = "FRM_Client_Define";
-  $hash->{InitFn}    = "FRM_RCOUT_Init";
+  $hash->{InitFn}    = "FRM_RCIN_Init";
   $hash->{UndefFn}   = "FRM_Client_Undef";
-  $hash->{AttrFn}    = "FRM_RCOUT_Attr";
+  $hash->{AttrFn}    = "FRM_RCIN_Attr";
   
   $hash->{AttrList}  = "IODev " . join(" ", keys %attributes) . " $main::readingFnAttributes";
   main::LoadModule("FRM");
 }
 
 sub
-FRM_RCOUT_Init($$)
+FRM_RCIN_Init($$)
 {
   my ($hash, $args) = @_;
-  my $ret = FRM_Init_Pin_Client($hash, $args, PIN_RCOUTPUT);
+  my $ret = FRM_Init_Pin_Client($hash, $args, PIN_RCINPUT);
   return $ret if (defined $ret);
   my $pin = $hash->{PIN};
   eval {
     my $firmata = FRM_Client_FirmataDevice($hash);
-    $firmata->observe_rc($pin, \&FRM_RCOUT_observer, $hash);
+    $firmata->observe_rc($pin, \&FRM_RCIN_observer, $hash);
   };
   return FRM_Catch($@) if $@;
   main::readingsSingleUpdate($hash, "state", "Initialized", 1);
@@ -67,7 +61,7 @@ FRM_RCOUT_Init($$)
 }
 
 sub
-FRM_RCOUT_Attr($$$$) {
+FRM_RCIN_Attr($$$$) {
   my ($command,$name,$attribute,$value) = @_;
   my $hash = $main::defs{$name};
   eval {
@@ -84,11 +78,10 @@ FRM_RCOUT_Attr($$$$) {
         defined($attributes{$attribute}) and do {
           if ($main::init_done) {
           	$main::attr{$name}{$attribute}=$value;
-            FRM_RCOUT_apply_attribute($hash,$attribute);
+            FRM_RCIN_apply_attribute($hash,$attribute);
           }
           last;
         };
-        
       }
     }
   };
@@ -103,32 +96,30 @@ FRM_RCOUT_Attr($$$$) {
 # The attribute is not applied within this module; instead, it is sent to the
 # microcontroller. When the change was successful, a response message will
 # arrive in the observer sub.
-sub FRM_RCOUT_apply_attribute {
+sub FRM_RCIN_apply_attribute { # TODO this one is identical to FRM_RCOUT_apply_attribute, merge
   my ($hash,$attribute) = @_;
   my $name = $hash->{NAME};
 
   return "Unknown attribute $attribute, choose one of " . join(" ", sort keys %attributes)
   	if(!defined($attributes{$attribute}));
 
-  FRM_Client_FirmataDevice($hash)->rcoutput_set_parameter($hash->{PIN},
-                                                          $attributes{$attribute},
-                                                          $main::attr{$name}{$attribute});
+  FRM_Client_FirmataDevice($hash)->rc_set_parameter($hash->{PIN},
+                                                    $attributes{$attribute},
+                                                    $main::attr{$name}{$attribute});
 }
 
-sub FRM_RCOUT_observer
+sub FRM_RCIN_observer
 {
   my ( $key, $value, $hash ) = @_;
   my $name = $hash->{NAME};
   
-  my %s = reverse(%sets);
   my %a = reverse(%attributes);
   
 COMMAND_HANDLER: {
-    defined($s{$key}) and do {
-      my %tristateChars = reverse(%tristateBits);
-      my $tristateCode = join("", map { my $v = $tristateChars{$_}; defined $v ? $v : "X";} @$value); 
-      Log3 $name, 4, "$s{$key}: $tristateCode";
-      main::readingsSingleUpdate($hash, $s{$key}, $tristateCode, 1);
+    ($key eq $Device::Firmata::Protocol::RCINPUT_COMMANDS->{RCINPUT_MESSAGE}) and do {
+      # TODO decode data
+      Log3 $name, 4, "Received RC message: $value";
+      main::readingsSingleUpdate($hash, 'state', $value, 1);
       last;
     };
     defined($a{$key}) and do {
@@ -142,67 +133,44 @@ COMMAND_HANDLER: {
 };
 }
 
-sub
-FRM_RCOUT_Set($@)
-{
-  my ($hash, @a) = @_;
-  return "Need at least 2 parameters" if(@a < 2);
-  return "Unknown argument $a[1], choose one of " . join(" ", sort keys %sets)
-  	if(!defined($sets{$a[1]}));
-  my $command = $a[1];
-  my $value = uc($a[2]);
-  
-  my @v = map {$tristateBits{$_}} split("", $value); 
-
-  my %s = reverse(%sets);
-  eval {
-    if ($sets{$command} eq $Device::Firmata::Protocol::RCOUTPUT_COMMANDS->{RCOUTPUT_CODE_TRISTATE}) {
-      FRM_Client_FirmataDevice($hash)->rcoutput_send_code_tristate($hash->{PIN}, @v);
-    }
-  };
-  return $@;
-}
-
 1;
 
 =pod
 =begin html
 
-<a name="FRM_RCOUT"></a>
-<h3>FRM_RCOUT</h3>
+<a name="FRM_RCIN"></a>
+<h3>FRM_RCIN</h3>
 <ul>
   represents a pin of an <a href="http://www.arduino.cc">Arduino</a> running <a href="http://www.firmata.org">Firmata</a>
-  configured to send data via the RCSwitch library.<br>
+  configured to receive data via the RCSwitch library.<br>
   Requires a defined <a href="#FRM">FRM</a>-device to work.<br><br> 
   
-  <a name="FRM_RCOUTdefine"></a>
+  <a name="FRM_RCINdefine"></a>
   <b>Define</b>
   <ul>
-  <code>define &lt;name&gt; FRM_RCOUT &lt;pin&gt;</code> <br>
-  Defines the FRM_RCOUT device. &lt;pin&gt> is the arduino-pin to use.
+  <code>define &lt;name&gt; FRM_RCIN &lt;pin&gt;</code> <br>
+  Defines the FRM_RCIN device. &lt;pin&gt> is the arduino-pin to use.
   </ul>
   
   <br>
-  <a name="FRM_RCOUTset"></a>
+  <a name="FRM_RCINset"></a>
   <b>Set</b><br>
   <ul>
-  <code>set &lt;name&gt; code &lt;code&gt;</code><br>sends a tristate coded message, e.g. <code>00FFF FF0FF F0<code> <br/> 
+  N/A
   </ul>
-  <a name="FRM_RCOUTget"></a>
+  <a name="FRM_RCINget"></a>
   <b>Get</b><br>
   <ul>
   N/A
   </ul><br>
-  <a name="FRM_RCOUTattr"></a>
+  <a name="FRM_RCINattr"></a>
   <b>Attributes</b><br>
   <ul>
       <li><a href="#IODev">IODev</a><br>
       Specify which <a href="#FRM">FRM</a> to use. (Optional, only required if there is more
       than one FRM-device defined.)
       </li>
-      <li>protocol</li>
-      <li>pulseLength</li>
-      <li>repeatTransmit</li>
+      <li>tolerance Receive tolerance (in percent)</li>
       <li><a href="#eventMap">eventMap</a><br></li>
       <li><a href="#readingFnAttributes">readingFnAttributes</a><br></li>
     </ul>
