9f4c7ae100a6cd72bc4f3e295d0fd2dad15a7bf0
git-developer <j@ckware.net>
2014-05-16 06:05:50 +0200

    Documentation, minor review

--- fhem/FHEM/20_FRM_RCOUT.pm
+++ fhem/FHEM/20_FRM_RCOUT.pm
@@ -61,6 +61,13 @@ FRM_RCOUT_Init($$)
   eval {
     my $firmata = FRM_Client_FirmataDevice($hash);
     $firmata->observe_rc($pin, \&FRM_RCOUT_observer, $hash);
+    my $name = $hash->{NAME};
+    FRM_RCOUT_Attr("set", $name, "defaultBitCount", $attributes{"defaultBitCount"});
+    foreach my $attribute (keys %attributes) { # send attribute values to the board
+      if ($main::attr{$name}{$attribute}) {
+        FRM_RCIN_apply_attribute($hash, $attribute);
+      }
+    }
   };
   return FRM_Catch($@) if $@;
   readingsSingleUpdate($hash, "state", "Initialized", 1);
@@ -69,22 +76,22 @@ FRM_RCOUT_Init($$)
 
 sub
 FRM_RCOUT_Attr($$$$) {
-  my ($command,$name,$attribute,$value) = @_;
+  my ($command, $name, $attribute, $value) = @_;
   my $hash = $main::defs{$name};
   eval {
     if ($command eq "set") {
       ARGUMENT_HANDLER: {
         $attribute eq "IODev" and do {
           if ($main::init_done and (!defined ($hash->{IODev}) or $hash->{IODev}->{NAME} ne $value)) {
-            FRM_Client_AssignIOPort($hash,$value);
+            FRM_Client_AssignIOPort($hash, $value);
             FRM_Init_Client($hash) if (defined ($hash->{IODev}));
           }
           last;
         };
         
         defined($attributes{$attribute}) and do {
+          $main::attr{$name}{$attribute} = $value;
           if ($main::init_done) {
-          	$main::attr{$name}{$attribute}=$value;
             FRM_RCOUT_apply_attribute($hash,$attribute);
           }
           last;
@@ -112,15 +119,15 @@ sub FRM_RCOUT_apply_attribute {
   	if(!defined($attributes{$attribute}));
 
   if ($attribute ne "defaultBitCount") {
-    FRM_Client_FirmataDevice($hash)->rc_set_parameter($hash->{PIN},
-                                                      $attributes{$attribute},
+    FRM_Client_FirmataDevice($hash)->rc_set_parameter($attributes{$attribute},
+                                                      $hash->{PIN},
                                                       $main::attr{$name}{$attribute});
   }
 }
 
 sub FRM_RCOUT_observer
 {
-  my ( $key, $value, $hash ) = @_;
+  my ( $key, $data, $hash ) = @_;
   my $name = $hash->{NAME};
   
   my %s = reverse(%sets);
@@ -131,19 +138,20 @@ sub FRM_RCOUT_observer
 COMMAND_HANDLER: {
     defined($subcommand) and do {
       if ("tristateCode" eq $subcommand) {
-        my $tristateCode = shift @$value;
-        Log3 $name, 5, "$subcommand: $tristateCode";
+        my $tristateCode = shift @$data;
+        Log3 $name, 4, "$subcommand: $tristateCode";
         readingsSingleUpdate($hash, $subcommand, $tristateCode, 1);
       } elsif ("longCode" eq $subcommand) {
-        my $bitlength = shift @$value;
-        my $longCode  = shift @$value;
-        Log3 $name, 5, "$subcommand: $longCode";
+        my $bitCount = shift @$data;
+        my $longCode  = shift @$data;
+        Log3 $name, 4, "$subcommand: $longCode/$bitCount";
         readingsBeginUpdate($hash);
         readingsBulkUpdate($hash, $subcommand, $longCode);
-        readingsBulkUpdate($hash, "bitlength", $bitlength);
+        readingsBulkUpdate($hash, "bitCount", $bitCount);
         readingsEndUpdate($hash, 1);
       } elsif ("charCode" eq $subcommand || "tristateString" eq $subcommand) {
-        my $charCode = shift @$value; 
+        my $charCode = shift @$data; 
+        Log3 $name, 4, "$subcommand: $charCode";
         readingsSingleUpdate($hash, $subcommand, $charCode, 1);
       } else {
         readingsSingleUpdate($hash, "state", "unknown subcommand $subcommand", 1);
@@ -151,7 +159,7 @@ COMMAND_HANDLER: {
       last;
     };
     defined($attrName) and do {
-      $value = shift @$value;
+      my $value = shift @$data;
       Log3 $name, 4, "$attrName: $value";
 
       $main::attr{$name}{$attrName}=$value;
@@ -177,7 +185,7 @@ FRM_RCOUT_Set($@)
       my $value = $a[2];
       my $bitCount = $a[3];
       $bitCount = $attr{$hash->{NAME}}{"defaultBitCount"} if not defined $bitCount;
-      $bitCount = 24 if not defined $bitCount;
+      $bitCount = $attributes{"defaultBitCount"} if not defined $bitCount; # if defaultBitCount was deleted
       @code = ($bitCount, $value);
     } elsif ($command eq $Device::Firmata::Protocol::RCOUTPUT_COMMANDS->{RCOUTPUT_CODE_CHAR}) {
         @code = map {ord($_)} split("", $a[2]);
@@ -194,44 +202,71 @@ FRM_RCOUT_Set($@)
 
 <a name="FRM_RCOUT"></a>
 <h3>FRM_RCOUT</h3>
-<ul>
-  represents a pin of an <a href="http://www.arduino.cc">Arduino</a> running <a href="http://www.firmata.org">Firmata</a>
-  configured to send data via the RCSwitch library.<br>
-  Requires a defined <a href="#FRM">FRM</a>-device to work.<br><br> 
-  
-  <a name="FRM_RCOUTdefine"></a>
-  <b>Define</b>
+  <p>
+    Represents a pin of an <a href="http://www.arduino.cc">Arduino</a> running
+    <a href="http://www.firmata.org">Firmata</a> configured to send data via the
+    RCSwitch library.<br/>
+    Requires a defined <a href="#FRM">FRM</a>-device to work.
+  </p>
+  <a name="FRM_RCOUTdefine" />
+  <h4>Define</h4>
+  <p>
+    <code>define &lt;name&gt; FRM_RCOUT &lt;pin&gt;</code><br/>
+    Defines the FRM_RCOUT device. &lt;pin&gt; is the arduino-pin to use.
+  </p>
+  <a name="FRM_RCOUTset" />
+  <h4>Set</h4>
   <ul>
-  <code>define &lt;name&gt; FRM_RCOUT &lt;pin&gt;</code> <br>
-  Defines the FRM_RCOUT device. &lt;pin&gt> is the arduino-pin to use.
+   <li>
+     <code>set &lt;name&gt; tristateCode &lt;code&gt;</code><br/>
+     Sends a tristate code, e.g. <code>00F0FF0FFF0F<code>
+   </li>
+   <li>
+     <code>set &lt;name&gt; longCode &lt;code&gt; &lt;bitCount&gt;</code><br/>
+     Sends a code as long decimal, e.g. <code>282961 24<code>.
+     The first argument gives a bit pattern as long (32 bits),
+     the second argument gives the number of bits that will actually be sent.
+     For example, <code>5 8</code> will be sent as <code>0000 0101</code>
+     whereas <code>5 4</code> will be sent as <code>0101</code>.<br/>
+     The second argument may be omitted, in this case the attribute
+     <code>defaultBitCount</code> is used. If this is not set, the default value
+     of 24 is used.
+   </li>
+   <li>
+     <code>set &lt;name&gt; charCode &lt;code&gt;</code><br/>
+     sends a character code, e.g. <code>001011011101<code>
+   </li>
   </ul>
-  
-  <br>
-  <a name="FRM_RCOUTset"></a>
-  <b>Set</b><br>
+  <a name="FRM_RCOUTget" />
+  <h4>Get</h4>
+    N/A
+  <br/>
+  <a name="FRM_RCOUTattr" />
+  <h4>Attributes</h4>
   <ul>
-  <code>set &lt;name&gt; code &lt;code&gt;</code><br>sends a tristate coded message, e.g. <code>00FFF FF0FF F0<code> <br/> 
+    <li>
+      <a href="#IODev">IODev</a><br/>
+      Specify which <a href="#FRM">FRM</a> to use.
+    </li>
+    <li>
+      <code>protocol</code>: RCSwitch parameter <code>protocol</code>
+      (default: 1; see RCSwitch for details)
+    </li>
+    <li>
+      <code>pulseLength: RCSwitch parameter <code>pulseLength</code>
+      (default: 350; see RCSwitch for details)</code>
+    </li>
+    <li>
+      <code>repeatTransmit: RCSwitch parameter <code>repeatTransmit</code>
+      (default: 10; see RCSwitch for details)</code>
+    </li>
+    <li>
+      <code>defaultBitCount: Default for the <code>bitCount</code> parameter
+      of the <code>longCode</code> command (default: 24) 
+    </li>
+    <li><a href="#eventMap">eventMap</a><br></li>
+    <li><a href="#readingFnAttributes">readingFnAttributes</a><br></li>
   </ul>
-  <a name="FRM_RCOUTget"></a>
-  <b>Get</b><br>
-  <ul>
-  N/A
-  </ul><br>
-  <a name="FRM_RCOUTattr"></a>
-  <b>Attributes</b><br>
-  <ul>
-      <li><a href="#IODev">IODev</a><br>
-      Specify which <a href="#FRM">FRM</a> to use. (Optional, only required if there is more
-      than one FRM-device defined.)
-      </li>
-      <li>protocol</li>
-      <li>pulseLength</li>
-      <li>repeatTransmit</li>
-      <li><a href="#eventMap">eventMap</a><br></li>
-      <li><a href="#readingFnAttributes">readingFnAttributes</a><br></li>
-    </ul>
-  </ul>
-<br>
 
 =end html
 =cut
