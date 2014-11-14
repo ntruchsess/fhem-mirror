f8179f485bcc4896107b0d693996fbc94a65585e
ntruchsess <norbert.truchsess@t-online.de>
2014-11-14 16:04:26 +0100

    websocket: oo-refactoring of fhem.js

--- fhem/www/fhem.js
+++ fhem/www/fhem.js
@@ -1,99 +1,56 @@
-var	fhem = {
+function fhem() {
 
-	connected: false,
+	var con = null;
+	var fhem = this;
 
-	con: null,
+	fhem.connected = false;
+	fhem.devices = {};
+	fhem.ondebug = null;
+	fhem.onerror = null;
+	fhem.onconnected = null;
+	fhem.ondisconnected = null;
+	fhem.onevent = null;
+	fhem.onlist = null;
 
-	devices: {},
-	
-	ondebug: null,
-	
-	onerror: null,
-	
-	onconnected: null,
-	
-	ondisconnected: null,
-	
-	onevent: null,
-	
-	onlist: null,
-	
-	connect: function(address,port) {
-		var con = new WebSocket('ws://'+address+':'+port, ['json']);
-
-		con.onopen = function() {
-			fhem.connected = true;
-			fhem.debug('Connection opened to fhem server!');
-			if (fhem.onconnected!=null) {
-				fhem.onconnected();
-			}
-		};
-
-		con.onclose = function() {
-			fhem.connected = false;
-			fhem.debug('Connection closed to fhem server!');
-			if (fhem.ondisconnected!=null) {
-				fhem.ondisconnected();
-			}
-		};
-
-		con.onerror = function(e) {
-			fhem.error('Websocket error ' + e.data + '.');
-		};
-
-		con.onmessage = function(e) {
-			fhem.debug("receiving data: "+e.data);
-			var msg = JSON.parse(e.data);
-			fhem.debug("receiving message: "+JSON.stringify(msg));
-
-			switch(msg.type) {
-			case 'event':
-				fhem.onEvent(msg.payload);
-				break;
-			case 'listentry':
-				fhem.onListentry(msg.payload);
-				break;
-			case 'commandreply':
-				fhem.onCommand(msg.payload);
-				break;
-			default:
-			}
-		};
-
-		fhem.con = con;
-	},
-
-	disconnect: function() {
-		fhem.con.close();
-	},
-
-	debug: function(m) {
+	function debug(m) {
 		if (fhem.ondebug != null) {
 			fhem.ondebug(m);
 		}
-	},
+	};
 	
-	error: function(m) {
+	function error(m) {
 		if (fhem.onerror != null) {
 			fhem.onerror(m);
 		}
-	},
+	};
 	
-	onEvent: function(event) {
+	function onEvent(event) {
+		if (!fhem.devices[event.name]) {
+			fhem.devices[event.name] = {
+				internals:  {},
+				readings:   {},
+				attributes: {},
+				sets:       {},
+				gets:       {},
+				attrList:   {}
+			};
+		}
 		for(key in event.changed) {
 			if (key == 'STATE') {
 				fhem.devices[event.name].internals['STATE'] = event.changed[key];
 			} else {
-				fhem.devices[event.name].readings[key].value = event.changed[key];
-				fhem.devices[event.name].readings[key].time  = event.time;
+				fhem.devices[event.name].readings[key] = {
+					value: event.changed[key],
+					time:  event.time
+				};
 			}
 		}
 		if (fhem.onevent != null) {
 			fhem.onevent();
 		}
-	},
+	};
 
-	onListentry: function(entry) {
+	function onListentry(entry) {
 		fhem.devices[entry.name] = {
 			internals:  entry.internals,
 			readings:   entry.readings,
@@ -105,27 +62,79 @@ var	fhem = {
 		if ((entry.index+1 == entry.num) && (fhem.onlist != null)) {
 			fhem.onlist();
 		}
-	},
+	};
 
-	onCommand: function(cmd) {
+	function onCommand(cmd) {
 		switch(cmd.command) {
 		case 'not implemented yet':
 			break;
 		default:
 		}
+	};
+	
+	fhem.connect = function(address,port) {
+
+		if (fhem.connected) {
+			fhem.disconnect();
+		}
+		
+		fhem.con = new WebSocket('ws://'+address+':'+port, ['json']);
+
+		fhem.con.onopen = function() {
+			fhem.connected = true;
+			debug('Connection opened to fhem server!');
+			if (fhem.onconnected!=null) {
+				fhem.onconnected();
+			}
+		};
+
+		fhem.con.onclose = function() {
+			fhem.connected = false;
+			debug('Connection closed to fhem server!');
+			if (fhem.ondisconnected!=null) {
+				fhem.ondisconnected();
+			}
+		};
+
+		fhem.con.onerror = function(e) {
+			fhem.error('Websocket error ' + e.data + '.');
+		};
+
+		fhem.con.onmessage = function(e) {
+			debug("receiving data: "+e.data);
+			var msg = JSON.parse(e.data);
+			debug("receiving message: "+JSON.stringify(msg));
+
+			switch(msg.type) {
+			case 'event':
+				onEvent(msg.payload);
+				break;
+			case 'listentry':
+				onListentry(msg.payload);
+				break;
+			case 'commandreply':
+				onCommand(msg.payload);
+				break;
+			default:
+			}
+		};
 	},
 
-	sendCommand: function(cmd) {
+	fhem.disconnect = function() {
+		fhem.con.close();
+	};
+
+	fhem.sendCommand = function(cmd) {
 		fhem.con.send(JSON.stringify({
 			type: 'command',
 			payload: cmd
 		}));
-	},
+	};
 		
-	subscribeAll: function() {
+	fhem.subscribeAll = function() {
 		fhem.sendCommand({
 			command: 'subscribe',
 			arg: 'all'
 		});
-	}
+	};
 };
