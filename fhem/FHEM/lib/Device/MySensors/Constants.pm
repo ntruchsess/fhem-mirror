50d4116a392a5fdb6ba2e167f97799d40bd2a054
Kolbi <MatthiasKolb@2-wheel-planet.de>
2015-09-11 16:00:03 +0200

    Update Constants.pm
    Updated to MySensors v 1.5

--- fhem/FHEM/lib/Device/MySensors/Constants.pm
+++ fhem/FHEM/lib/Device/MySensors/Constants.pm
@@ -40,33 +40,43 @@ use constant {
   V_KWH         => 18,
   V_SCENE_ON    => 19,
   V_SCENE_OFF   => 20,
-  V_HEATER      => 21,
-  V_HEATER_SW   => 22,
-  V_LIGHT_LEVEL => 23,
-  V_VAR1        => 24,
-  V_VAR2        => 25,
-  V_VAR3        => 26,
-  V_VAR4        => 27,
-  V_VAR5        => 28,
-  V_UP          => 29,
-  V_DOWN        => 30,
-  V_STOP        => 31,
-  V_IR_SEND     => 32,
-  V_IR_RECEIVE  => 33,
-  V_FLOW        => 34,
-  V_VOLUME      => 35,
-  V_LOCK_STATUS => 36,
-  V_DUST_LEVEL	=> 37,
-  V_VOLTAGE	    => 38,
-  V_CURRENT     => 39,
+  V_LIGHT_LEVEL => 21,
+  V_VAR1        => 22,
+  V_VAR2        => 23,
+  V_VAR3        => 24,
+  V_VAR4        => 25,
+  V_VAR5        => 26,
+  V_UP          => 27,
+  V_DOWN        => 28,
+  V_STOP        => 29,
+  V_IR_SEND     => 30,
+  V_IR_RECEIVE  => 31,
+  V_FLOW        => 32,
+  V_VOLUME      => 33,
+  V_LOCK_STATUS => 34,
+  V_VOLTAGE	    => 35,
+  V_CURRENT     => 36,
+  V_STATUS      => 37,
+  V_PERCENTAGE  => 38,
+  V_HVAC_FLOW_STATE => 39,
+  V_HVAC_SPEED  => 40,
+  V_LEVEL       => 41,
+  V_RGB         => 42,
+  V_RGBW        => 43,
+  V_ID          => 44,
+  V_UNIT_PREFIX => 45, 
+  V_HVAC_SETPOINT_COOL => 46,
+  V_HVAC_SETPOINT_HEAT => 47,
+  V_HVAC_FLOW_MODE => 48,
 };
 
 use constant variableTypes => qw{ V_TEMP V_HUM V_LIGHT V_DIMMER V_PRESSURE V_FORECAST V_RAIN
         V_RAINRATE V_WIND V_GUST V_DIRECTION V_UV V_WEIGHT V_DISTANCE
         V_IMPEDANCE V_ARMED V_TRIPPED V_WATT V_KWH V_SCENE_ON V_SCENE_OFF
-        V_HEATER V_HEATER_SW V_LIGHT_LEVEL V_VAR1 V_VAR2 V_VAR3 V_VAR4 V_VAR5
+        V_LIGHT_LEVEL V_VAR1 V_VAR2 V_VAR3 V_VAR4 V_VAR5
         V_UP V_DOWN V_STOP V_IR_SEND V_IR_RECEIVE V_FLOW V_VOLUME V_LOCK_STATUS 
-        V_DUST_LEVEL V_VOLTAGE V_CURRENT};
+        V_VOLTAGE V_CURRENT V_STATUS V_PERCENTAGE V_HVAC_FLOW_STATE V_HVAC_SPEED 
+        V_LEVEL V_RGB V_RGBW V_ID V_UNIT_PREFIX V_HVAC_SETPOINT_COOL V_HVAC_SETPOINT_HEAT V_HVAC_FLOW_MODE};
 
 sub variableTypeToStr($) {
   (variableTypes)[shift];
@@ -133,11 +143,24 @@ use constant {
   S_CUSTOM                => 23,
   S_DUST                  => 24,
   S_SCENE_CONTROLLER      => 25,
+  S_BINARY                => 26,
+  S_RGB_LIGHT             => 27,
+  S_RGBW_LIGHT            => 28,
+  S_COLOR_SENSOR          => 29,
+  S_HVAC                  => 30,
+  S_MULTIMETER            => 31,
+  S_SPRINKLER             => 32,
+  S_WATER_LEAK            => 33,	
+  S_SOUND                 => 34,	
+  S_VIBRATION             => 35,
+  S_MOISTURE              => 36,
 };
 
 use constant sensorTypes => qw{ S_DOOR S_MOTION S_SMOKE S_LIGHT S_DIMMER S_COVER S_TEMP S_HUM S_BARO S_WIND
         S_RAIN S_UV S_WEIGHT S_POWER S_HEATER S_DISTANCE S_LIGHT_LEVEL S_ARDUINO_NODE
-        S_ARDUINO_REPEATER_NODE S_LOCK S_IR S_WATER S_AIR_QUALITY S_CUSTOM S_DUST S_SCENE_CONTROLLER };
+        S_ARDUINO_REPEATER_NODE S_LOCK S_IR S_WATER S_AIR_QUALITY S_CUSTOM S_DUST S_SCENE_CONTROLLER S_BINARY
+        S_RGB_LIGHT S_RGBW_LIGHT S_COLOR_SENSOR S_HVAC S_MULTIMETER S_SPRINKLER S_WATER_LEAK S_SOUND S_VIBRATION
+        S_MOISTURE};
 
 sub sensorTypeToStr($) {
   (sensorTypes)[shift];
